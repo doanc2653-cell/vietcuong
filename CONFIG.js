1a129867f378665abed02bbf53b7b4d5e3cadebb
doanc2653-cell <doanc2653@gmail.com>
2026-01-12 22:10:52 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
-  titleHeader: "Chào cậu, tớ có điều muốn nói. </br> Làm người yêu tớ nhé!",
+  titleHeader: "Đại ka ơi,em có điều muốn nói. </br> Cho em xinli nhee!",
   descriptionHeader:
-    "Hãy trả lời một cách thật lòng nhé. Nếu cậu im lặng và rời đi thì tức là cậu đã ĐỒNG Ý.",
-  buttonYes: "Tớ đồng ý",
+    "Hãy trả lời một cách thật lòng nhé. Nếu đại ka im lặng và rời đi tức là ĐỒNG Ý.",
+  buttonYes: "Đồng ý",
   buttonNo: "Không đồng ý",
-  titleModar: "Cảm ơn cậu.",
+  titleModar: "Cảm ơn đại ka nha.",
   descriptionModar:
-    "Cảm ơn cậu đã cho tớ một cơ hội để tớ có thể ở bên cạnh, che chở và bảo vệ cậu. </br> Tớ biết tớ không được đẹp trai, cũng chẳng lắm tài nhưng tớ có một trái tim chân thành. Cậu chính là cả thế giới của tớ. </br> Tối nay tớ sẽ đến đón cậu. Cậu mặc thật đẹp vào nhé! Sẽ có một bất ngờ dành cho cậu.</br> Love you <3",
+    "Cảm ơn cậu đka nhé lần sau em sẽ không thế nữa. </br> Xinloi nhá nhá nhá </br> Tối nay tớ sẽ đến đón cậu. Bỏ chặn đi nhé .</br> huhu",
 };
